5dcef7f4e2b70898dc72a7a2f89ca7d5e9646f8f
Scott Hudson <shudson5523@gmail.com>
2018-09-11 06:22:31 -0400

    finished counter exercise

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( <nav className="navbar navbar-light bg-light">
+//Stateless Functional Components
+
+const NavBar = ({ totalCounters }) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 h1">Navbar</span>
         <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
+            {totalCounters}
         </span>
-      </nav> );
-    }
-}
- 
+      </nav> 
+    );
+};
+
 export default NavBar;
